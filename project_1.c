cf1797d4dd15a3956167a51fe6a527a5559e30d2
ATHUL <138679015+AK-120@users.noreply.github.com>
2023-10-28 08:21:13 +0530

    Update project_1.c

--- project_1.c
+++ project_1.c
@@ -7,19 +7,19 @@ struct DC_Cafe_Item {
     int quantity;
 };
 
-// Function to display the menu and prices
+
 void displayMenu() {
     printf("Welcome to DC CAFE\n");
     printf("Menu:\n");
-    printf("1. Tea - $2.00\n");
-    printf("2. Coffee - $2.50\n");
-    printf("3. Espresso - $3.00\n");
-    printf("4. Americano - $2.75\n");
-    printf("5. Cappuccino - $3.50\n");
-    printf("6. Cookies - $1.50\n");
-    printf("7. Brownies - $2.00\n");
-    printf("8. Donuts - $1.75\n");
-    printf("9. Danish Pastries - $3.25\n");
+    printf("1. Tea - ₹120.00\n");
+    printf("2. Coffee - ₹150.00\n");
+    printf("3. Espresso - ₹180.00\n");
+    printf("4. Americano - ₹165.00\n");
+    printf("5. Cappuccino - ₹210.00\n");
+    printf("6. Cookies - ₹90.00\n");
+    printf("7. Brownies - ₹120.00\n");
+    printf("8. Donuts - ₹105.00\n");
+    printf("9. Danish Pastries - ₹195.00\n");
 }
 
 int main() {
@@ -46,44 +46,42 @@ int main() {
             i--;
             continue;
         }
-
-        // Set item details based on user's choice
         switch (choice) {
             case 1:
                 strcpy(items[i].name, "Tea");
-                items[i].price = 2.00;
+                items[i].price = 120.00;
                 break;
             case 2:
                 strcpy(items[i].name, "Coffee");
-                items[i].price = 2.50;
+                items[i].price = 150.00;
                 break;
             case 3:
                 strcpy(items[i].name, "Espresso");
-                items[i].price = 3.00;
+                items[i].price = 180.00;
                 break;
             case 4:
                 strcpy(items[i].name, "Americano");
-                items[i].price = 2.75;
+                items[i].price = 165.00;
                 break;
             case 5:
                 strcpy(items[i].name, "Cappuccino");
-                items[i].price = 3.50;
+                items[i].price = 210.00;
                 break;
             case 6:
                 strcpy(items[i].name, "Cookies");
-                items[i].price = 1.50;
+                items[i].price = 90.00;
                 break;
             case 7:
                 strcpy(items[i].name, "Brownies");
-                items[i].price = 2.00;
+                items[i].price = 120.00;
                 break;
             case 8:
                 strcpy(items[i].name, "Donuts");
-                items[i].price = 1.75;
+                items[i].price = 105.00;
                 break;
             case 9:
                 strcpy(items[i].name, "Danish Pastries");
-                items[i].price = 3.25;
+                items[i].price = 195.00;
                 break;
         }
 
@@ -93,14 +91,12 @@ int main() {
         scanf("%d", &items[i].quantity);
 
         double itemAmount = items[i].price * items[i].quantity;
-
         totalBill += itemAmount;
     }
 
     printf("\nEnter customer's name: ");
     scanf("%s", customerName);
 
-    // Confirmation step for the entire order
     printf("\nConfirm your order (Y/N): ");
     char confirmation;
     scanf(" %c", &confirmation);
@@ -109,9 +105,9 @@ int main() {
         printf("\nDC CAFE - Cafe Bill for %s:\n", customerName);
         printf("----------------------------\n");
         for (int i = 0; i < numItems; i++) {
-            printf("%s (Quantity: %d) - $%.2lf\n", items[i].name, items[i].quantity, items[i].price * items[i].quantity);
+            printf("%s (Quantity: %d) - ₹%.2lf\n", items[i].name, items[i].quantity, items[i].price * items[i].quantity);
         }
-        printf("\nTotal Bill: $%.2lf\n", totalBill);
+        printf("\nTotal Bill: ₹%.2lf\n", totalBill);
         printf("----------------------------\n");
     } else {
         printf("\nOrder canceled. No bill generated.\n");
